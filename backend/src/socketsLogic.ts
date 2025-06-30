93e72f9b793654446a579e84ae5034f8abe3c455
Lucian Volker Irsigler <l.irsigler64@gmail.com>
2025-06-30 12:26:25 +0200

    restructure

---
+++ backend/src/socketsLogic.ts
@@ -0,0 +1,38 @@
+import { Server } from "socket.io";
+import { Server as HTTPServer } from "http";
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+
+export function createNewServer(httpServer:HTTPServer){
+    const io = new Server<
+        ClientToServerEvents,
+        ServerToClientEvents,
+        InterServerEvents,
+        SocketData
+    >(httpServer, {
+      cors:{
+        origin:"http://localhost:5500",
+        methods: ["GET", "POST"]
+      }
+    });
+
+    return io;
+}
